32a8acf3d01ba829804b707ac880f576748fd94a
Tamil Selvan R <tamilselvan@example.com>
2025-04-16 11:09:10 +0530

    Update Mpongo_URL

--- config/db.js
+++ config/db.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.CONNECTION_STRING);
+    await mongoose.connect(process.env.CONNECTION_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB Connected Successfully!");
   } catch (err) {
     console.error("MongoDB connection failed:", err.message);
